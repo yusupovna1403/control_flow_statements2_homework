94cf8b9ab91fa247a206291f214ae5bbe9b35ce1
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-02 14:01:38 +0500

    if founction05

--- if05.py
+++ if05.py
@@ -6,4 +6,25 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    d1 = n % 10
+    n//=10
+    d2 = n % 10
+    n//=10
+    d3 = n % 10
+    n//=10
+    d4 = n % 10
+    n//=10
+    d5 = n % 10
+    max = d1
+    
+    if d2 > d1:
+        return d2
+    elif d3 > d1:
+        return d3
+    elif d4 > d1:
+        return d4
+    elif d5 > d1:
+        return d5
+    else:
+        return d1
+print(main(23546))
